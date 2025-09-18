d5788bc27ad2e9b357185102aa42abb74cb90f7f
va-vae <2278024346@qq.com>
2025-09-18 17:49:23 +0800

    隐藏指定区域建筑及给指定区域添加新的建筑

--- src/views/cesiumDemo/cesium2.vue
+++ src/views/cesiumDemo/cesium2.vue
@@ -1,11 +1,13 @@
 <template>
   <div ref="cesiumContainer2"></div>
+  <button class="toggle-building" @click="toggleBuilding">切换建筑</button>
 </template>
 
 <script setup lang="ts">
 import { onMounted, onUnmounted, ref } from 'vue';
 import * as Cesium from 'cesium';
 import "cesium/Build/Cesium/Widgets/widgets.css";
+import { color } from 'echarts';
 
 const cesiumContainer2 = ref<HTMLDivElement>();
 const viewer = ref<Cesium.Viewer>();
@@ -34,14 +36,17 @@ const initializeViewer = async() => {
   viewer.value.camera.flyTo({
     destination: Cesium.Cartesian3.fromDegrees(-104.9965, 39.74248, 4000),
   });
-  addOSMBuildings();
-  addBuildingGeoJSON();
+  await addOSMBuildings();
+  await addBuildingGeoJSON();
+  hideBuildings();
+  addNewBuilding();
 }
 // 添加OSM建筑
+let buildingsTileset = ref(null);
 const addOSMBuildings = async () => {
   if(!cesiumContainer2.value) return;
-  const buildingsTileset = await Cesium.createOsmBuildingsAsync();
-  viewer.value.scene.primitives.add(buildingsTileset);
+  buildingsTileset.value = await Cesium.createOsmBuildingsAsync();
+  viewer.value.scene.primitives.add(buildingsTileset.value);
 }
 
 
@@ -60,6 +65,41 @@ const addBuildingGeoJSON = async() => {
   viewer.value.flyTo(dataSource)
 }
 
+// 5 在该区域中用3d Tiles隐藏不想展示的建筑
+const hideBuildings = () => {
+  buildingsTileset.value.style = new Cesium.Cesium3DTileStyle({
+    show: {
+      conditions: [
+        // 包含下方id的都隐藏
+        ['${elementId} === 332469316', false],
+        ['${elementId} === 332469317', false],
+        ['${elementId} === 235368665', false],
+        ['${elementId} === 530288180', false],
+        ['${elementId} === 530288179', false],
+        // 不包含的则显示
+        [true, true]
+      ]
+    },
+    // 设置颜色
+    color: "Boolean(${feature['cesium#color']}) ? color(${feature['cesium#color']}) : color('#ffffff')"
+  })
+}
+
+// 6 将新建筑添加到场景中
+let newBuildingTileset = ref(null)
+const addNewBuilding = async() => {
+  newBuildingTileset.value = await Cesium.Cesium3DTileset.fromIonAssetId(3753965);
+  viewer.value?.scene.primitives.add(newBuildingTileset.value);
+
+  // 移动相机至新建筑处
+  viewer.value?.flyTo(newBuildingTileset.value);
+}
+
+// 7 添加建筑切换事件
+const toggleBuilding = () => {
+  newBuildingTileset.value.show = !newBuildingTileset.value.show
+}
+
 onMounted(() => {
   initializeViewer()
 }),
@@ -70,4 +110,6 @@ onUnmounted(() => {
 });
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+ .toggle-building { z-index: 5; position: fixed; top: 5px; left: 5px; }
+ </style>
